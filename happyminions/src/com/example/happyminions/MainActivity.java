6f0bf7982a2005600e0b0edf1ad82f62d5a17e8b
trisha <kotharitrisha@gmail.com>
2013-07-28 02:54:51 -0400

    testing

--- happyminions/src/com/example/happyminions/MainActivity.java
+++ happyminions/src/com/example/happyminions/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.happyminions;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.EditText;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	public final static String EXTRA_MESSAGE = "com.example.happyminions.MESSAGE";
+	public final static String TAG = "HappyMinionsLogger";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +35,80 @@ public class MainActivity extends Activity {
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         // Do something in response to button
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	/*Intent intent = new Intent(this, DisplayMessageActivity.class);
     	EditText editText = (EditText) findViewById(R.id.edit_message);
     	String message = editText.getText().toString();
     	intent.putExtra(EXTRA_MESSAGE, message);
     	startActivity(intent);
+    	*/
+    	if (SpeechRecognizer.isRecognitionAvailable(getApplicationContext())){
+    		Log.i("LOGGER", "Yes info avaialble");
+    		SpeechRecognizer sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+    		sr.setRecognitionListener(new listener()); 
+    		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);       
+    		sr.startListening(intent);
+    	}
     }
+    
+    class listener implements RecognitionListener          
+    {
+             public void onReadyForSpeech(Bundle params)
+             {
+                      Log.d(TAG, "onReadyForSpeech");
+             }
+             public void onBeginningOfSpeech()
+             {
+                      Log.d(TAG, "onBeginningOfSpeech");
+             }
+             public void onRmsChanged(float rmsdB)
+             {
+                      Log.d(TAG, "onRmsChanged");
+             }
+             public void onBufferReceived(byte[] buffer)
+             {
+                      Log.d(TAG, "onBufferReceived");
+             }
+             public void onEndOfSpeech()
+             {
+                      Log.d(TAG, "onEndofSpeech");
+             }
+             public void onError(int error)
+             {
+                      Log.d(TAG,  "error " +  error);
+             }
+             public void onResults(Bundle results)                   
+             {
+                      String str = new String();
+                      Log.d(TAG, "onResults " + results);
+                      ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                      for (int i = 0; i < data.size(); i++)
+                      {
+                                //Log.d(TAG, "result " + data.get(i));
+                                str += data.get(i);
+                                Log.d(TAG, "result " + str); 
+                      }             }
+             public void onPartialResults(Bundle partialResults)
+             {
+                      Log.d(TAG, "onPartialResults");
+             }
+             public void onEvent(int eventType, Bundle params)
+             {
+                      Log.d(TAG, "onEvent " + eventType);
+             }
+    }
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+		if (SpeechRecognizer.isRecognitionAvailable(getApplicationContext())){
+    		Log.i("LOGGER", "Yes info avaialble");
+    		SpeechRecognizer sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+    		sr.setRecognitionListener(new listener()); 
+    		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);       
+    		sr.startListening(intent);
+    	}
+		
+	}
     
 }
