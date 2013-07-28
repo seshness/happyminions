50d9a6da9ee1a74faa17001f8a78e3dc37680c3d
Seshadri Mahalingam <seshadri@berkeley.edu>
2013-07-27 17:57:41 -0700

    Adds fetching happy texts

--- server/app.js
+++ server/app.js
@@ -33,7 +33,8 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.post('/text', text.new);
+app.post('/text', text.create);
+app.get('/happytexts', text.happyTexts);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- server/routes/text.js
+++ server/routes/text.js
@@ -4,14 +4,22 @@ var Text = require('../models/Text');
  * POST text
  */
 
-exports.new = function(req, res) {
+exports.create = function(req, res) {
   var text = new Text({
     text: req.body.text,
     start_time: req.body.start_time,
     end_time: req.body.end_time,
-    sentiment: null
+    sentiment: req.body.DEBUG === 'TRUE' ? req.body.sentiment : null
   });
   text.save(function(err, text) {
     res.send(200, { "id": text.id });
+  });
+};
+
+exports.happyTexts = function(req, res) {
+  Text.where('sentiment').gt(0.5).exec(function(err, texts) {
+    res.send(200, {
+      "happyTexts": texts
+    });
   });
 };
