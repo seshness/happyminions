c114cfff0fcee1c6517210738896d58b911e14e6
Seshadri Mahalingam <seshadri@berkeley.edu>
2013-07-27 17:19:17 -0700

    fix a misunderstanding between req.params and req.body

--- server/routes/text.js
+++ server/routes/text.js
@@ -6,9 +6,9 @@ var Text = require('../models/Text');
 
 exports.new = function(req, res) {
   var text = new Text({
-    text: req.params.text,
-    start_time: req.params.start_time,
-    end_time: req.params.end_time,
+    text: req.body.text,
+    start_time: req.body.start_time,
+    end_time: req.body.end_time,
     sentiment: null
   });
   text.save(function(err, text) {
